e7d312d0e874e7492bd13b1096b216c0eef8bf9a
Joao Carlos Vieira <joaocv3@Gmail.com>
2017-02-16 09:58:53 -0200

    Sixth Class

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,16 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute']).
+config(function($routeProvider, $locationProvider){
+
+	$locationProvider.html5Mode(true); // allow route to be written without hash: http://localhost:3000/fotos instead of http://localhost:3000/#/fotos 
+
+	$routeProvider.when('/fotos',{
+		templateUrl: 'partials/principal.html',
+		controller: 'FotosController'
+	});
+
+	$routeProvider.when('/fotos/new',{
+		templateUrl: 'partials/foto.html'
+	});
+
+	$routeProvider.otherwise({ redirectTo: '/fotos'});
+});
