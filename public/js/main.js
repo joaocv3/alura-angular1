a6a60c46dd21f72c73c8e015420f988ea40b50e2
Joao Carlos Vieira <joaocv3@Gmail.com>
2017-02-17 11:33:14 -0200

    Seventh Class

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,18 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute']).
-config(function($routeProvider, $locationProvider){
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+.config(function($routeProvider, $locationProvider) {
 
-	$locationProvider.html5Mode(true); // allow route to be written without hash: http://localhost:3000/fotos instead of http://localhost:3000/#/fotos 
+		$locationProvider.html5Mode(true); // allow route to be written without hash: http://localhost:3000/fotos instead of http://localhost:3000/#/fotos 
 
-	$routeProvider.when('/fotos',{
-		templateUrl: 'partials/principal.html',
-		controller: 'FotosController'
+		$routeProvider.when('/fotos', {
+			templateUrl: 'partials/principal.html',
+			controller: 'FotosController'
+		});
+
+		$routeProvider.when('/fotos/new', {
+			templateUrl: 'partials/foto.html',
+			controller: 'FotoController'
+		});
+
+		$routeProvider.otherwise({redirectTo: '/fotos'});
+
 	});
-
-	$routeProvider.when('/fotos/new',{
-		templateUrl: 'partials/foto.html'
-	});
-
-	$routeProvider.otherwise({ redirectTo: '/fotos'});
-});
